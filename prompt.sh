917cfcce17cc4ed0fd79ecb1cbdc0766036cba72
Gin-san <gin.san.rzlk.g@gmail.com>
2020-12-28 14:39:28 +0100

    Update prompt.sh

--- prompt.sh
+++ prompt.sh
@@ -1,6 +1,20 @@
 source $HOME/.default-term-setup/git-prompt.sh
+export GIT_PS1_SHOWDIRTYSTATE=1
+
+__custom_prompt() {
+    local prompt_git="$(__git_ps1)"
+    if [[ "$prompt_git" ]]; then
+        prompt_git=$(echo "$prompt_git" | sed 's/.*(\(.*\))/\1/' | sed 's/\(.*\) \(.*\)/\1 \2/')
+        printf -- '─[\033[93m%s'$color1']' "$prompt_git"
+    fi
+}
+
 if [[ ${EUID} == 0 ]]; then
-    export PS1='\[\033[01;31m\]\h\[\033[01;34m\] \W\[\033[01;33m\]$(__git_ps1)\[\033[01;34m\] \$\[\033[00m\] '
+    color1='\033[95m'
+    color2='\033[1;31m'
 else
-    export PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \w\[\033[01;33m\]$(__git_ps1)\[\033[01;34m\] \$\[\033[00m\] '
+    color1='\033[94m'
+    color2='\033[1;34m'
 fi
+
+export PS1='\['$color1'\]┌──(\[\033[00m\]\['$color2'\]\u@\h\[\033[00m\]\['$color1'\])─[\[\033[00m\]\w\['$color1'\]]$(__custom_prompt)\n\['$color1'\]│\n└─\['$color2'\]$\[\033[00m\] '
